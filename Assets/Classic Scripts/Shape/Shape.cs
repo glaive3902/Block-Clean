da3d0380f81c71563e008572d5367a00476775cd
glaive3902 <nguyendung3902@gmail.com>
2024-05-28 17:27:23 +0700

    push

--- Assets/Classic Scripts/Shape/Shape.cs
+++ Assets/Classic Scripts/Shape/Shape.cs
@@ -138,21 +138,122 @@ public class Shape : MonoBehaviour, IPointerClickHandler, IPointerUpHandler, IBe
                 }
             }
         }
-    }
-    public float GetXPositionForShapeSquare(ShapeData shapeData, int column, Vector2 moveDistance)
-    {
-		float squareShiftX = 0;
-		int middleElement = shapeData.columns / 2;
-		squareShiftX = moveDistance.x * (middleElement - column);
-		return squareShiftX;
+    }	
+    /*
+    private float GetYPositionForShapeSquare(ShapeData shapeData, int row, Vector2 moveDistance)
+	{
+		float shiftOnY = 0f;
+		if (shapeData.rows > 1)
+		{
+			if (shapeData.rows % 2 != 0)
+			{
+				var middleSquareIndex = (shapeData.rows - 1) / 2;
+				var multiplier = (shapeData.rows - 1) / 2;
+				if (row < middleSquareIndex)
+				{
+					shiftOnY = moveDistance.y * 1;
+					shiftOnY *= multiplier;
+				}
+				else if (row > middleSquareIndex)
+				{
+					shiftOnY = moveDistance.y * - 1;
+					shiftOnY *= multiplier;
+				}
+			}
+			else
+			{
+				var middleSquareIndex2 = (shapeData.rows == 2) ? 1 : (shapeData.rows / 2);
+				var middleSquareIndex1 = (shapeData.rows == 2) ? 0 : shapeData.rows - 2;
+				var multiplier = shapeData.rows / 2;
+
+				if (row == middleSquareIndex1 || row == middleSquareIndex2)
+				{
+					if (row == middleSquareIndex2)
+						shiftOnY = (moveDistance.y / 2) * -1;
+					if (row == middleSquareIndex1)
+						shiftOnY = (moveDistance.y / 2);
+				}
+
+				if (row < middleSquareIndex1 && row < middleSquareIndex2)
+				{
+					shiftOnY = moveDistance.y * 1;
+					shiftOnY *= multiplier;
+				}
+				else if (row > middleSquareIndex1 && row > middleSquareIndex2)
+				{
+					shiftOnY = moveDistance.y * - 1;
+					shiftOnY *= multiplier;
+				}
+			}
+		}
+		return shiftOnY;
 	}
+    
+    private float GetXPositionForShapeSquare(ShapeData shapeData, int column, Vector2 moveDistance)
+    {
+		float shiftOnX = 0f;
+		if(shapeData.columns > 1)
+        {
+            if(shapeData.columns % 2 != 0)
+            {
+                var middleSquareIndex = (shapeData.columns - 1) / 2;
+                var multiplier = (shapeData.columns - 1) / 2;
+                if(column < middleSquareIndex)
+                {
+                    shiftOnX = moveDistance.x * -1;
+                    shiftOnX *= multiplier;
+                }
+                else if (column > middleSquareIndex)
+                {
+                    shiftOnX = moveDistance.x * 1;
+                    shiftOnX *= multiplier;
+                }
+            }
+            else
+            {
+                var middleSquareIndex2 = (shapeData.columns == 2) ? 1 : (shapeData.columns / 2);
+                var middleSquareIndex1 = (shapeData.columns == 2) ? 0 : (shapeData.columns -1);
+                var multiplier = shapeData.columns / 2;
+
+                if(column == middleSquareIndex1 || column == middleSquareIndex2)
+                {
+                    if (column == middleSquareIndex2)
+                        shiftOnX = moveDistance.x / 2;
+                    if (column == middleSquareIndex1)
+                        shiftOnX = (moveDistance.x / 2) * -1;
+                }
+
+                if(column < middleSquareIndex1 && column < middleSquareIndex2)
+                {
+                    shiftOnX = moveDistance.x * -1;
+                    shiftOnX *= multiplier;
+                }
+                else if(column > middleSquareIndex1 && column > middleSquareIndex2)
+                {
+                    shiftOnX = moveDistance.x * 1;
+                    shiftOnX *= multiplier;
+                }
+            }
+        }
+        return shiftOnX;
+	}
+    */
+	//*
 	public float GetYPositionForShapeSquare(ShapeData shapeData, int row, Vector2 moveDistance)
 	{
 		float squareShiftY = 0;
-		int middleElement = shapeData.rows / 2;
+		int middleElement = (shapeData.rows / 2);
 		squareShiftY = moveDistance.y * (middleElement - row);
 		return squareShiftY;
 	}
+	public float GetXPositionForShapeSquare(ShapeData shapeData, int column, Vector2 moveDistance)
+	{
+		float squareShiftX = 0;
+		int middleElement = (shapeData.columns / 2);
+		squareShiftX = moveDistance.x * (middleElement - column) * -1;
+		return squareShiftX;
+	}
+	//*/
 	private int GetNumberOfSquares(ShapeData shapeData)
     {
         int number = 0;
